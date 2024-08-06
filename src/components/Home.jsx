d7c38c33b297edda8e0bc0e01cab852414f36ed7
alicansadeler <alicansadeler@gmail.com>
2024-08-06 17:28:35 +0300

    homebanner change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,18 +2,14 @@ import { Link } from "react-router-dom";
 import { HomeLink } from "./HomeLink";
 import React from "react";
 import { HomePage } from "./HomePage";
-import { HomePageLink } from "./HomePageLink";
+import homeBanner from "./Assets/mile1-assets/home-banner.png";
 
 export default function Home() {
   return (
     <>
       <div className="home-container">
         <div className="home-item0">
-          <img
-            src="./Assets/mile1-assets/home-banner.png"
-            alt="Home Banner"
-            className="home-item1"
-          />
+          <img src={homeBanner} alt="Home Banner" className="home-item1" />
           <div className="home-item2">
             <p className="home-text2">fırsatı kaçırma</p>
             <p className="home-text1">
